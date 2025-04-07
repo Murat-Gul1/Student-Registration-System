2cef1b623ccde2d4ed870b9e6a0519a917091284
Murat Gül <murat.coddev@gmail.com>
2025-04-07 19:21:49 +0300

    Add inline comments to Student and StudentManager classes

--- OgrenciKayit/src/Student.java
+++ OgrenciKayit/src/Student.java
@@ -1,40 +1,50 @@
+// This class represents a student with basic personal and academic information.
 public class Student {
+    // The student's first name
     private String firstName;
+    // The student's last name
     private String lastName;
+    // The student's unique ID number
     private int id;
+    // The student's grade (0-100 scale)
     private double grade;
-
+    // Default constructor
     public Student(){
 
     }
-
+    // Sets the student's first name
     public void setFirstName (String firstName){
         this.firstName = firstName;
     }
+    // Returns the student's first name
     public String getFirstName(){
         return this.firstName;
     }
-
+    // Sets the student's last name
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
+    // Returns the student's last name
     public String getLastName(){
         return this.lastName;
     }
-
+    // Sets the student's ID
     public void setId(int id){
         this.id = id;
     }
+    // Returns the student's ID
     public int getId(){
         return this.id;
     }
-
+    // Sets the student's grade
     public void setGrade(double grade){
         this.grade = grade;
     }
+    // Returns the student's grade
     public double getGrade(){
         return this.grade;
     }
+    // Returns a string representation of the student's information
     @Override
     public String toString(){
         return "Id =" + id +
--- OgrenciKayit/src/StudentManager.java
+++ OgrenciKayit/src/StudentManager.java
@@ -1,8 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
 public class StudentManager {
+    // List that holds all student objects
     private List<Student> studentList;
-
+    // Constructor that initializes the student list
     public StudentManager(){
         studentList = new ArrayList<>();
     }
@@ -10,6 +11,9 @@ public class StudentManager {
     public void addStudent(Student student){
         studentList.add(student);
     }
+    /**
+     * Prints all students in the list using their toString() method.
+     */
     public void printStudent(){
         for(Student  student : studentList){
             System.out.println(student);
